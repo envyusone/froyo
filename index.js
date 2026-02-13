971f0d7b83e522a650a6de7076859c7ac4be4ba5
envyusone <abriche123@gmail.com>
2026-02-12 20:09:10 -0700

    quick

---
+++ index.js
@@ -0,0 +1,12 @@
+let userInputString = prompt(
+  "Please enter a list of comma-separated froyo flavors:", 
+  "vanilla,vanilla,vanilla,strawberry,coffee,coffee"
+);
+
+let stringArray = userInputString.split(",");
+
+
+}
+
+
+
